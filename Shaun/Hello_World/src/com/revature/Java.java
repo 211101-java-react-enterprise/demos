87e7f12c04b9e58d0f98033c0053a249c4f42b71
Shaun Nord <shaun_nord@hotmail.com>
2021-11-03 16:23:16 -0600

    Setting up personal branch

---
+++ Shaun/Hello_World/src/com/revature/Java.java
@@ -0,0 +1,23 @@
+// This is our package declaration, specifying the extact location of this class within our project.
+package com.revature.hello_world;
+
+// This is a single line comment
+
+// This is a "top-level" class declaration, because it is declared as "public"
+// The top-level class's name within a source code file MUST match exactly to the file name (minus the file extension)
+public class HelloWorld {
+
+    // Primary entrypoint of a standard Java application
+    public static void main(String[] args) {
+        
+        // Prints a phrase to the console.
+        System.out.println("Hello, world!");
+        
+    }
+    
+    // Allowed variation of the main method
+    // public static void main(String... args) {
+    // 
+    // }
+
+}
