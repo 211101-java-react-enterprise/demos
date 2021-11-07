90789cc047e145b028b364b68c3a1ef4ad2c2e7b
Pamela <p.samantha32@protonmail.com>
2021-11-07 10:07:07 -0600

    basic hello_world from command line

---
+++ java/hello-world/src/com/revature/hello_world/HelloWorld.java
@@ -0,0 +1,21 @@
+// This is our package declaration, specifying the exact location of this class within our project
+package com.revature.hello_world;
+
+// This is a "top-level" class declaration, because it is declared as "public"
+// The top-level class's name within a source code file MUST match exactly to the file name (minus the file extension)
+
+
+public class HelloWorld {
+
+	//Primary entrypoint of a standard Java applicatoin 
+	public static void main(String[] args) {
+	
+	//Prints a phrase to the console
+	System.out.println("Hello, World!");
+	
+
+	}
+
+	
+
+}
