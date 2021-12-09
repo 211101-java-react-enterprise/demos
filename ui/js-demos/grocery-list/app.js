116922c66f0e96af3e8187136fa47a1413e11262
JonahLandry <ebonytalon721@gmail.com>
2021-12-09 16:00:59 -0500

    Finished the initial version of our list

--- ui/js-demos/grocery-list/app.js
+++ ui/js-demos/grocery-list/app.js
@@ -1,3 +1,5 @@
+
+// Adds an item to the list
 function addGroceryDataToTable () {
     // Allocate element references
     let newItemField = document.getElementById('new-item');
@@ -23,5 +25,46 @@ function addGroceryDataToTable () {
             alert('Input a value!');
         }
 }
+
+// Testing a tutorial
+let dragged;
+let id;
+let index;
+let indexDrop;
+let list;
+
+  document.addEventListener("dragstart", ({target}) => {
+      dragged = target;
+      id = target.id;
+      list = target.parentNode.children;
+      for(let i = 0; i < list.length; i += 1) {
+        if(list[i] === dragged){
+          index = i;
+        }
+      }
+  });
+
+  document.addEventListener("dragover", (event) => {
+      event.preventDefault();
+  });
+
+  document.addEventListener("drop", ({target}) => {
+   if(target.className == "dropzone" && target.id !== id) {
+       dragged.remove( dragged );
+      for(let i = 0; i < list.length; i += 1) {
+        if(list[i] === target){
+          indexDrop = i;
+        }
+      }
+      console.log(index, indexDrop);
+      if(index > indexDrop) {
+        target.before( dragged );
+      } else {
+       target.after( dragged );
+      }
+    }
+  });
+
+
 
 document.getElementById('add-item').addEventListener('click', addGroceryDataToTable);
