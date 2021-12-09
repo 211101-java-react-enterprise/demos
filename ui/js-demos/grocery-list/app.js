2f99335ea6e88fbb7c2805635e1891b047e6792d
unknown <hoyzington@gmail.com>
2021-12-09 15:50:43 -0600

    more implementation

--- ui/js-demos/grocery-list/app.js
+++ ui/js-demos/grocery-list/app.js
@@ -1,42 +1,30 @@
-// function* idGenerator() {
-//     let idCounter = 1;
-//     while(true) {
-//         yield idCounter++;
-//     }
-// }
-
-// let idGen = idGenerator();
-
 function addItemToGroceryList() {
-
     let itemField = document.getElementById('new-item');
-    
     let item = itemField.value;
-
     if (item) {
-
         let li = document.createElement('li');
         li.innerText = item;
-        li.addEventListener('click', moveItemToPurchased)
-
+        li.addEventListener('click', moveItemToPurchList)
         let ul = document.getElementById('grocery-list-items');
         ul.appendChild(li);
-
-
         itemField.value = '';
-
     } else {
         alert('You need to provide an item!');
     }
 }
 
-function moveItemToPurchased(params) {
-    let li = document.createElement('li');
-    li.innerText = this.innerText;
-    let ul = document.getElementById('purchased-items');
-    ul.appendChild(li);
+function moveItemToGrocList() {
     let grocList = document.getElementById('grocery-list-items');
-    
+    grocList.appendChild(this);
+    this.addEventListener('click', moveItemToPurchList);
+    this.removeEventListener('click', moveItemToGrocList);
+}
+
+function moveItemToPurchList() {
+    let purchList = document.getElementById('purchased-items');
+    purchList.appendChild(this);
+    this.addEventListener('click', moveItemToGrocList);
+    this.removeEventListener('click', moveItemToPurchList);
 }
 
 document.getElementById('add-item').addEventListener('click', addItemToGroceryList);
