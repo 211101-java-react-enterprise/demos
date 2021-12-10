d1d2587c09edc38bc1f28c42279d239ec15fc408
cjsssusa <cjsssusa@gmail.com>
2021-12-09 21:58:06 -0500

    js-quiz update

--- ui/js-demos/js-quiz/app.js
+++ ui/js-demos/js-quiz/app.js
@@ -1,11 +1,14 @@
+let buttonDiv;
+let submitTxt;
 window.onload = () => {
     console.log('The window has loaded!');
 
     // Dynamic creation of HTML elements needed for the page
     let quizDiv = document.createElement('div');
     let resultsDiv = document.createElement('div');
-    let buttonDiv = document.createElement('div');
+    buttonDiv = document.createElement('div');
     let submitBtn = document.createElement('button');
+    submitTxt = document.createElement('span');
 
     // Set attributes on the newly create elements
     quizDiv.setAttribute('id', 'quiz');
@@ -15,12 +18,13 @@ window.onload = () => {
     buttonDiv.style.cssFloat = 'left';
     submitBtn.setAttribute('id', 'submit');
     submitBtn.setAttribute('class', 'btn btn-primary');
-    
+
     // Add some text to display within the submit button
     submitBtn.innerText = 'Submit Quiz';
 
     // Add the configured elements to the body of the page
     buttonDiv.appendChild(submitBtn);
+    buttonDiv.appendChild(submitTxt);
     document.body.prepend(resultsDiv);
     document.body.prepend(buttonDiv);
     document.body.prepend(quizDiv);
@@ -29,6 +33,7 @@ window.onload = () => {
     buttonDiv.addEventListener('mouseover', isQuizValid);
     submitBtn.addEventListener('click', gradeQuiz);
 
+    buttonDiv.blur();
     buildQuiz();
 
 }
@@ -52,7 +57,7 @@ function buildQuiz() {
         for (let letter in currentQuestion.answers) {
             answers.push(`
             <input type="radio" name="question-${index}" value="${letter}"/>
-            ${letter}: ${currentQuestion.answers[letter]}
+            <span>${letter}: ${currentQuestion.answers[letter]}</span>
             <br/>`);
         }
 
@@ -67,20 +72,71 @@ function buildQuiz() {
     // Combine our output array's contents into a single string of HTML and put it on the page
     quizContainer.innerHTML = output.join('');
 
+    //let tf = isQuizValid();
+    //console.log(tf);
 
+}
+
+
+function isQuizValid_check(index) {
+    let els = document.querySelectorAll("input[type='radio'][name='question-" + index + "']");
+
+    for (let idx = 0; idx < els.length; idx++) {
+        if (els[idx].checked) {
+            return true;
+        }
+    }
+    return false;
 }
 
 
 function isQuizValid() {
 
+    for (let index = 0; index < quizQuestions.length; index++) {
+        let tf = isQuizValid_check(index);
+        if (!tf) {
+            console.log(false);
+            return false;
+        }
+    };
+    console.log(true);
+    return true;
 }
 
 function gradeQuiz() {
 
+    if (!isQuizValid())
+        return;
+
+    let ccc = 0;
+    let x = 0;
+    for (let index = 0; index < quizQuestions.length; index++) {
+        let els = document.querySelectorAll("input[type='radio'][name='question-" + index + "']");
+        for (let idx = 0; idx < els.length; idx++) {
+
+            if (els[idx].checked) {
+                let next = els[idx].nextElementSibling;
+                if (els[idx].value == (quizQuestions[index].correctAnswer)) {
+                    console.log("match");
+                    next.style.color = 'green';
+                    console.log(ccc);
+                    ccc++;
+                    console.log(ccc);
+                } else {
+                    console.log("not match");
+                    next.style.color = 'red';
+                    x++;
+                }
+                break;
+            }
+        }
+        console.log(ccc);
+        submitTxt.innerHTML = " " + ccc + "/" + quizQuestions.length + " : " + (ccc / quizQuestions.length * 100);
+    };
+
 }
 
-let quizQuestions = [
-    {
+let quizQuestions = [{
         question: 'Which of the following is NOT a falsy value',
         answers: {
             a: '{}',
