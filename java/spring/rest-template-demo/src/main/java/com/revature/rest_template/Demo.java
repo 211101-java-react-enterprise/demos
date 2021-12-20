2440b276455c0189038e77e80858ceb4706fb09c
Wezley Singleton <wezley.singleton@gmail.com>
2021-12-20 17:03:01 -0500

    resttemplate end; with webclient bonus!

--- java/spring/rest-template-demo/src/main/java/com/revature/rest_template/Demo.java
+++ java/spring/rest-template-demo/src/main/java/com/revature/rest_template/Demo.java
@@ -1,9 +1,70 @@
 package com.revature.rest_template;
 
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.netty.http.client.HttpClient;
+
+import java.time.Duration;
+import java.util.HashMap;
+
 public class Demo {
 
     public static void main(String[] args) {
-        
+
+        // RestTemplate: The AJAX/Fetch of Spring
+
+        String apiUrl = "http://localhost:5000/quizzard";
+        RestTemplate client = new RestTemplate();
+
+        Credentials testUserCreds = new Credentials("test", "test");
+        ResponseEntity<Principal> response = client.postForEntity(apiUrl + "/auth", testUserCreds, Principal.class);
+
+        if (response.getStatusCode().value() != 200) {
+            throw new RuntimeException("Invalid credentials provided!");
+        }
+
+        String token = response.getHeaders().get("Authorization").get(0);
+        System.out.println(token);
+        System.out.println(response.getBody());
+
+        //---------------------------------------------------------------------------------------------------------------
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.set("Authorization" , token);
+        HttpEntity requestConfig = new HttpEntity(httpHeaders);
+
+        ResponseEntity<HashMap[]> questionsResponse = client.exchange(apiUrl + "/questions", HttpMethod.GET, requestConfig, HashMap[].class);
+        System.out.println(questionsResponse.getStatusCode());
+
+        for (HashMap question : questionsResponse.getBody()) {
+            System.out.println(question);
+        }
+
+        //--------------------------------------------------------------------------------
+
+        // WebClient: The Axios of Spring
+
+        HttpClient httpClient = HttpClient.create()
+                                          .responseTimeout(Duration.ofMillis(5000));
+
+        WebClient betterClient = WebClient.builder()
+                                          .baseUrl(apiUrl)
+                                          .clientConnector(new ReactorClientHttpConnector(httpClient))
+                                          .build();
+
+        Principal authUser = betterClient.post()
+                                         .uri("/auth")
+                                         .bodyValue(testUserCreds)
+                                         .retrieve()
+                                         .bodyToMono(Principal.class).block();
+
+        System.out.println(authUser);
+
     }
 
 }
---
+++ java/spring/rest-template-demo/src/main/java/com/revature/rest_template/Principal.java
@@ -0,0 +1,42 @@
+package com.revature.rest_template;
+
+public class Principal {
+
+    private String id;
+    private String username;
+    private String role;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "Principal{" +
+                "id='" + id + '\'' +
+                ", username='" + username + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
+
+}
